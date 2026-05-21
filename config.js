9c846f0fb7a5f49e3f635c1435b9159a33dc9e89
quyenbaby24-debug <quyenbaby24@gmail.com>
2026-05-21 14:42:53 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const APP_CONFIG = {
+    // Thay YOUR_DEPLOYMENT_ID bằng ID thực tế sau khi deploy
+    WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbwvyIAAQG_IufbbYumqOgLHjlKDiCXUm7VmeuCJjKRGjGlj1iJl9N1Ptg3M0t_vgC4/exec',
+    
+    // SHA-256 của "admin" (có thể thay bằng mật khẩu khác)
+    ADMIN_PASSWORD_HASH: '6051fc84a7a0d74c225fb18a496b09952da5642e60723ecae543298edd7d82d6',
+    
+    HOME_URL: 'index.html',
+    AUTO_REFRESH_MS: 12000,
+    DEFAULT_CT_FEEDBACK: 'Đã tiếp nhận ý kiến, chủ toạ sẽ trả lời khi kết luận'
+};
